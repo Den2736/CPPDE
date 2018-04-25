1d31be493bd3d9ee92b1f5c70ed0adb293578719
Елизавета Степанова <lectrabiosim@mail.ru>
2018-04-25 18:37:27 +0300

    Nodes types separated into two files

--- CPPDE/CPPDE/Models/NodeClasses.cs
+++ CPPDE/CPPDE/Models/NodeClasses.cs
@@ -33,7 +33,7 @@ namespace C__DE.Models
         WriteNode
     }
 
-    public abstract class Node
+    public abstract partial class Node
     {
         public NodeType TypeOfNode; //Тип узла
         public BlockNode parentBlock; //ссылка на родительский блок (именно блок - if, while, корневой и т.д.)
@@ -41,10 +41,9 @@ namespace C__DE.Models
         public abstract void SetParentBlock(BlockNode Parent); //установка родительского блока (рекурсивная процедура)
     }
 
-    public abstract class BlockNode : Node
+    public abstract partial class BlockNode : Node
     {
         public List<Node> ChildrenOperators; //Ссылки на все внутренние операторы
-        public List<Variable> BlockVariables; //Переменные блока
         public void AddOperator(Node Operator)//метод добавления оператора в список дочерних
         {
             ChildrenOperators.Add(Operator);
@@ -57,14 +56,13 @@ namespace C__DE.Models
         }
     }
 
-    public abstract class AtomNode : Node
+    public abstract partial class AtomNode : Node
     {
         public Variable MainVariable;
-        public abstract void SetMainVariable(Counters count);
         //в зависимости от типа узла ссылка на переменную в таблице или на временную переменную
     }
 
-    public class ConstantNode: AtomNode
+    public partial class ConstantNode: AtomNode
     {
         public string ConstantValue; //значение вне зависимости от типа будет храниться в строковом виде
         public ConstantNode(string Type, string ConstValue, int numLine)
@@ -81,14 +79,9 @@ namespace C__DE.Models
             parentBlock = Parent;
         }
 
-        public override void SetMainVariable(Counters count)
-        {
-            MainVariable.AlternativeName = "const_" + count.consts.ToString();
-            ++count.consts;
-        }
     }
 
-    public class VariableNode : AtomNode
+    public partial class VariableNode : AtomNode
     {
         public string VariableName;
         VariableNode(string Var, int numLine)
@@ -102,26 +95,9 @@ namespace C__DE.Models
         {
             parentBlock = Parent;
         }
-
-        public override void SetMainVariable(Counters count)
-        {
-            BlockNode parent = parentBlock; //ищем переменную в родительском блоке
-            while (true)
-            {
-                Variable possibleVariable = parent.BlockVariables.FirstOrDefault(var => var.Name == VariableName);
-                if (possibleVariable.Name != null)
-                {
-                    MainVariable = possibleVariable;
-                    return;
-                }
-                if (parent.TypeOfNode == NodeType.RootNode) //если добрались до родительского и не нашли, то переменная не объявлена
-                    throw new UndefinedVariableException(LineNumber, VariableName);
-                parent = parent.parentBlock;//идём дальше вверх по иерархии блоков
-            }
-        }
     }
 
-    public class BinaryOperatorNode: AtomNode //логический, сравнения или арифметический
+    public partial class BinaryOperatorNode: AtomNode //логический, сравнения или арифметический
     {
         //может быть и унарный - как частный случай
         public bool IsUnary;
@@ -154,172 +130,9 @@ namespace C__DE.Models
             if (!IsUnary)
                 SecondOperand.SetParentBlock(Parent);
         }
-
-        public void CheckTypesAdd()//если оператор плюс или минус
-        {
-            switch (FirstOperand.MainVariable.Type)
-            {
-                case "int":
-                    {
-                        switch (SecondOperand.MainVariable.Type)
-                        {
-                            case "int":{ MainVariable.Type = "int"; break;}
-                            case "float": { MainVariable.Type = "float"; break; }
-                            default: { throw new IncompatibleTypesException(LineNumber, "int", SecondOperand.MainVariable.Type); }
-                        }
-                        break;
-                    }
-                case "float":
-                    {
-                        switch (SecondOperand.MainVariable.Type)
-                        {
-                            case "int": { MainVariable.Type = "float"; break; }
-                            case "float": { MainVariable.Type = "float"; break; }
-                            default: { throw new IncompatibleTypesException(LineNumber, "int", SecondOperand.MainVariable.Type); }
-                        }
-                        break;
-                    }
-                case "string":
-                    {
-                        switch (SecondOperand.MainVariable.Type)
-                        {
-                            case "string": { MainVariable.Type = "string"; break; }
-                            case "char": { MainVariable.Type = "string"; break; }
-                            default: { throw new IncompatibleTypesException(LineNumber, "string", SecondOperand.MainVariable.Type); }
-                        }
-                        break;
-                    }
-                case "char":
-                    {
-                        switch (SecondOperand.MainVariable.Type)
-                        {
-                            case "string": { MainVariable.Type = "string"; break; }
-                            case "char": { MainVariable.Type = "string"; break; }
-                            default: { throw new IncompatibleTypesException(LineNumber, "string", SecondOperand.MainVariable.Type); }
-                        }
-                        break;
-                    }
-                case "bool":
-                    {
-                        throw new InvalidTypeException(LineNumber, "bool", Operator);
-                    }
-            }
-        }
-
-        public void CheckTypesMul() //умножение и деление
-        {
-            switch (FirstOperand.MainVariable.Type)
-            {
-                case "int":
-                    {
-                        switch (SecondOperand.MainVariable.Type)
-                        {
-                            case "int": { MainVariable.Type = "int"; break; }
-                            case "float": { MainVariable.Type = "float"; break; }
-                            default: { throw new IncompatibleTypesException(LineNumber, "int", SecondOperand.MainVariable.Type); }
-                        }
-                        break;
-                    }
-                case "float":
-                    {
-                        switch (SecondOperand.MainVariable.Type)
-                        {
-                            case "int": { MainVariable.Type = "float"; break; }
-                            case "float": { MainVariable.Type = "float"; break; }
-                            default: { throw new IncompatibleTypesException(LineNumber, "float", SecondOperand.MainVariable.Type); }
-                        }
-                        break;
-                    }
-                default:
-                    { throw new InvalidTypeException(LineNumber, FirstOperand.MainVariable.Type, Operator); }
-            }
-        }
-
-        public void CheckTypesComparison()//сравнивать можно число с числом, строку со строкой или bool с bool
-        {
-            string type1=FirstOperand.MainVariable.Type, type2= SecondOperand.MainVariable.Type;
-            if (((type1 == "int" || type1 == "float") && (type2 == "int" || type2 == "float"))//если оба числа
-                || ((type1 == "char" || type1 == "string") && (type2 == "char" || type2 == "string"))//или оба строковые
-                || (type1 == "bool" && type2 == "bool"))
-                MainVariable.Type = "bool";
-            else throw new IncompatibleTypesException(LineNumber, type1, type2);
-        }
-
-        public void CheckTypesBit()//побитовые операции, допустимы только целые числа и логические значения
-        {
-            if (FirstOperand.MainVariable.Type == "bool" && SecondOperand.MainVariable.Type == "bool")
-                MainVariable.Type = "bool";
-            else
-            if ((FirstOperand.MainVariable.Type == "int" || FirstOperand.MainVariable.Type == "bool") &&
-                (SecondOperand.MainVariable.Type == "int" || SecondOperand.MainVariable.Type == "bool"))
-                MainVariable.Type = "int";
-            else throw new IncompatibleTypesException(LineNumber, FirstOperand.MainVariable.Type, SecondOperand.MainVariable.Type);
-        }
-
-        public void CheckTypesLogical()//логические операции
-        {
-            string type1 = FirstOperand.MainVariable.Type, type2 = FirstOperand.MainVariable.Type;
-            if (((type1 == "int") || (type1 == "float") || (type1 == "bool")) || ((type2 == "int") || (type2 == "float") || (type2 == "bool")))
-                MainVariable.Type = "bool";
-            else throw new IncompatibleTypesException(LineNumber, FirstOperand.MainVariable.Type, SecondOperand.MainVariable.Type);
-        }
-        public override void SetMainVariable(Counters count)
-        {
-            MainVariable = new Variable();
-            MainVariable.AlternativeName = "temp_" + count.temps.ToString();
-            count.temps++;
-            FirstOperand.SetMainVariable(count);
-            if (IsUnary)
-            {
-                if (Operator == "+" || Operator == "-")
-                    if (FirstOperand.MainVariable.Type == "int" || FirstOperand.MainVariable.Type == "float")
-                    {
-                        MainVariable.Type = FirstOperand.MainVariable.Type; //если унарный, то тип совпадает с типом операнда
-                        return;
-                    }
-                    else throw new InvalidTypeException(LineNumber, FirstOperand.MainVariable.Type, Operator);
-                if (Operator == "!")
-                {
-                    if (FirstOperand.MainVariable.Type == "bool" || FirstOperand.MainVariable.Type == "int")
-                    {
-                        MainVariable.Type = FirstOperand.MainVariable.Type; //если унарный, то тип совпадает с типом операнда
-                        return;
-                    }
-                    else throw new InvalidTypeException(LineNumber, FirstOperand.MainVariable.Type, Operator);
-                }
-            }
-            else
-            {
-                SecondOperand.SetMainVariable(count);//дальше проверка совместимости типов и корректности операций
-                switch (TypeOfNode)
-                {
-                    case NodeType.ArithmeticOperator:
-                        { if (Operator == "+" || Operator == "-")
-                                CheckTypesAdd();
-                            else CheckTypesMul();
-                            break;
-                        }
-                    case NodeType.ComparisonOperator:
-                        {
-                            CheckTypesComparison();
-                            break;
-                        }
-                    case NodeType.BitOperator:
-                        {
-                            CheckTypesBit();
-                            break;
-                        }
-                    case NodeType.LogicalOperator:
-                        {
-                            CheckTypesLogical();
-                            break;
-                        }
-                }
-            }
-        }
     }
 
-    public class VariableDeclarationNode: AtomNode
+    public partial class VariableDeclarationNode: AtomNode
     {
         string Type;
         //при семантическом анализе поставить isDeclared=true у mainVariable
@@ -336,19 +149,9 @@ namespace C__DE.Models
             parentBlock = Parent;
             DeclaratedVariable.SetParentBlock(Parent);
         }
-        public override void SetMainVariable(Counters count)
-        {
-            MainVariable = new Variable(); //создаём новую переменную и наделяем её новыми свойствами
-            MainVariable.Name = DeclaratedVariable.VariableName;
-            MainVariable.AlternativeName = "var_" + count.vars.ToString();
-            count.vars++;
-            parentBlock.BlockVariables.Add(MainVariable); //помещаем в список переменных данного блока
-            DeclaratedVariable.SetMainVariable(count);
-        }
-
     }
 
-    public class ConditionalOperatorNode: BlockNode
+    public partial class ConditionalOperatorNode: BlockNode
     {
         public bool ExistedElse; //есть ли ветка else
         public AtomNode Condition;
@@ -383,7 +186,7 @@ namespace C__DE.Models
         }
     }
 
-    public class ConditionalBranchNode: BlockNode//какая-то из веток if или else
+    public partial class ConditionalBranchNode: BlockNode//какая-то из веток if или else
     {
         public ConditionalBranchNode()
         {
@@ -392,7 +195,7 @@ namespace C__DE.Models
         }
     }
 
-    public class CycleOperator: BlockNode
+    public partial class CycleOperator: BlockNode
     {
         public AtomNode BeginningActivity;
         public AtomNode ContinueCondition;
@@ -425,7 +228,7 @@ namespace C__DE.Models
 
     }
 
-    public class AssignmentOperator: AtomNode
+    public partial class AssignmentOperator: AtomNode
     {
         //оператор присваивания, сюда входят также операторы += -= и так далее
         public string AssignmentOperation; //сама операция (просто присваивание или ещё что-то)
@@ -446,13 +249,9 @@ namespace C__DE.Models
             AssignedVariable.SetParentBlock(Parent);
         }
 
-        public override void SetMainVariable(Counters count)
-        {
-           
-        }
     }
 
-    public class MainRootNode: BlockNode
+    public partial class MainRootNode: BlockNode
     {
         public MainRootNode(int numLine)
         {
@@ -462,7 +261,7 @@ namespace C__DE.Models
     }
 
     //С чтением и записью хз как, пока строка будет
-    public class ReadOperator: AtomNode
+    public partial class ReadOperator: AtomNode
     {
         public string Source;
         public VariableNode ReadVariable;
@@ -480,7 +279,7 @@ namespace C__DE.Models
         }
     }
 
-    public class WriteOperator : AtomNode
+    public partial class WriteOperator : AtomNode
     {
         public string Source;
         public VariableNode WriteVariable;
---
+++ CPPDE/CPPDE/Models/SemanticFunctions.cs
@@ -0,0 +1,270 @@
+﻿using C__DE.Models.Exceptions.SemanticExceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__DE.Models
+{
+    public abstract partial class Node
+    {
+    }
+
+    public abstract partial class BlockNode : Node
+    {
+        public List<Variable> BlockVariables; //Переменные блока
+    }
+
+    public abstract partial class AtomNode : Node
+    {
+        public abstract void SetMainVariable(Counters count);
+        //в зависимости от типа узла ссылка на переменную в таблице или на временную переменную
+    }
+
+    public partial class ConstantNode : AtomNode
+    {
+        public override void SetMainVariable(Counters count)
+        {
+            MainVariable.AlternativeName = "const_" + count.consts.ToString();
+            ++count.consts;
+        }
+    }
+
+    public partial class VariableNode : AtomNode
+    {
+        public override void SetMainVariable(Counters count)
+        {
+            BlockNode parent = parentBlock; //ищем переменную в родительском блоке
+            while (true)
+            {
+                Variable possibleVariable = parent.BlockVariables.FirstOrDefault(var => var.Name == VariableName);
+                if (possibleVariable.Name != null)
+                {
+                    MainVariable = possibleVariable;
+                    return;
+                }
+                if (parent.TypeOfNode == NodeType.RootNode) //если добрались до родительского и не нашли, то переменная не объявлена
+                    throw new UndefinedVariableException(LineNumber, VariableName);
+                parent = parent.parentBlock;//идём дальше вверх по иерархии блоков
+            }
+        }
+    }
+
+    public partial class BinaryOperatorNode : AtomNode //логический, сравнения или арифметический
+    {
+        public void CheckTypesAdd()//если оператор плюс или минус
+        {
+            switch (FirstOperand.MainVariable.Type)
+            {
+                case "int":
+                    {
+                        switch (SecondOperand.MainVariable.Type)
+                        {
+                            case "int": { MainVariable.Type = "int"; break; }
+                            case "float": { MainVariable.Type = "float"; break; }
+                            default: { throw new IncompatibleTypesException(LineNumber, "int", SecondOperand.MainVariable.Type); }
+                        }
+                        break;
+                    }
+                case "float":
+                    {
+                        switch (SecondOperand.MainVariable.Type)
+                        {
+                            case "int": { MainVariable.Type = "float"; break; }
+                            case "float": { MainVariable.Type = "float"; break; }
+                            default: { throw new IncompatibleTypesException(LineNumber, "int", SecondOperand.MainVariable.Type); }
+                        }
+                        break;
+                    }
+                case "string":
+                    {
+                        switch (SecondOperand.MainVariable.Type)
+                        {
+                            case "string": { MainVariable.Type = "string"; break; }
+                            case "char": { MainVariable.Type = "string"; break; }
+                            default: { throw new IncompatibleTypesException(LineNumber, "string", SecondOperand.MainVariable.Type); }
+                        }
+                        break;
+                    }
+                case "char":
+                    {
+                        switch (SecondOperand.MainVariable.Type)
+                        {
+                            case "string": { MainVariable.Type = "string"; break; }
+                            case "char": { MainVariable.Type = "string"; break; }
+                            default: { throw new IncompatibleTypesException(LineNumber, "string", SecondOperand.MainVariable.Type); }
+                        }
+                        break;
+                    }
+                case "bool":
+                    {
+                        throw new InvalidTypeException(LineNumber, "bool", Operator);
+                    }
+            }
+        }
+
+        public void CheckTypesMul() //умножение и деление
+        {
+            switch (FirstOperand.MainVariable.Type)
+            {
+                case "int":
+                    {
+                        switch (SecondOperand.MainVariable.Type)
+                        {
+                            case "int": { MainVariable.Type = "int"; break; }
+                            case "float": { MainVariable.Type = "float"; break; }
+                            default: { throw new IncompatibleTypesException(LineNumber, "int", SecondOperand.MainVariable.Type); }
+                        }
+                        break;
+                    }
+                case "float":
+                    {
+                        switch (SecondOperand.MainVariable.Type)
+                        {
+                            case "int": { MainVariable.Type = "float"; break; }
+                            case "float": { MainVariable.Type = "float"; break; }
+                            default: { throw new IncompatibleTypesException(LineNumber, "float", SecondOperand.MainVariable.Type); }
+                        }
+                        break;
+                    }
+                default:
+                    { throw new InvalidTypeException(LineNumber, FirstOperand.MainVariable.Type, Operator); }
+            }
+        }
+
+        public void CheckTypesComparison()//сравнивать можно число с числом, строку со строкой или bool с bool
+        {
+            string type1 = FirstOperand.MainVariable.Type, type2 = SecondOperand.MainVariable.Type;
+            if (((type1 == "int" || type1 == "float") && (type2 == "int" || type2 == "float"))//если оба числа
+                || ((type1 == "char" || type1 == "string") && (type2 == "char" || type2 == "string"))//или оба строковые
+                || (type1 == "bool" && type2 == "bool"))
+                MainVariable.Type = "bool";
+            else throw new IncompatibleTypesException(LineNumber, type1, type2);
+        }
+
+        public void CheckTypesBit()//побитовые операции, допустимы только целые числа и логические значения
+        {
+            if (FirstOperand.MainVariable.Type == "bool" && SecondOperand.MainVariable.Type == "bool")
+                MainVariable.Type = "bool";
+            else
+            if ((FirstOperand.MainVariable.Type == "int" || FirstOperand.MainVariable.Type == "bool") &&
+                (SecondOperand.MainVariable.Type == "int" || SecondOperand.MainVariable.Type == "bool"))
+                MainVariable.Type = "int";
+            else throw new IncompatibleTypesException(LineNumber, FirstOperand.MainVariable.Type, SecondOperand.MainVariable.Type);
+        }
+
+        public void CheckTypesLogical()//логические операции
+        {
+            string type1 = FirstOperand.MainVariable.Type, type2 = FirstOperand.MainVariable.Type;
+            if (((type1 == "int") || (type1 == "float") || (type1 == "bool")) || ((type2 == "int") || (type2 == "float") || (type2 == "bool")))
+                MainVariable.Type = "bool";
+            else throw new IncompatibleTypesException(LineNumber, FirstOperand.MainVariable.Type, SecondOperand.MainVariable.Type);
+        }
+        public override void SetMainVariable(Counters count)
+        {
+            MainVariable = new Variable();
+            MainVariable.AlternativeName = "temp_" + count.temps.ToString();
+            count.temps++;
+            FirstOperand.SetMainVariable(count);
+            if (IsUnary)
+            {
+                if (Operator == "+" || Operator == "-")
+                    if (FirstOperand.MainVariable.Type == "int" || FirstOperand.MainVariable.Type == "float")
+                    {
+                        MainVariable.Type = FirstOperand.MainVariable.Type; //если унарный, то тип совпадает с типом операнда
+                        return;
+                    }
+                    else throw new InvalidTypeException(LineNumber, FirstOperand.MainVariable.Type, Operator);
+                if (Operator == "!")
+                {
+                    if (FirstOperand.MainVariable.Type == "bool" || FirstOperand.MainVariable.Type == "int")
+                    {
+                        MainVariable.Type = FirstOperand.MainVariable.Type; //если унарный, то тип совпадает с типом операнда
+                        return;
+                    }
+                    else throw new InvalidTypeException(LineNumber, FirstOperand.MainVariable.Type, Operator);
+                }
+            }
+            else
+            {
+                SecondOperand.SetMainVariable(count);//дальше проверка совместимости типов и корректности операций
+                switch (TypeOfNode)
+                {
+                    case NodeType.ArithmeticOperator:
+                        {
+                            if (Operator == "+" || Operator == "-")
+                                CheckTypesAdd();
+                            else CheckTypesMul();
+                            break;
+                        }
+                    case NodeType.ComparisonOperator:
+                        {
+                            CheckTypesComparison();
+                            break;
+                        }
+                    case NodeType.BitOperator:
+                        {
+                            CheckTypesBit();
+                            break;
+                        }
+                    case NodeType.LogicalOperator:
+                        {
+                            CheckTypesLogical();
+                            break;
+                        }
+                }
+            }
+        }
+    }
+
+    public partial class VariableDeclarationNode : AtomNode
+    {
+        public override void SetMainVariable(Counters count)
+        {
+            MainVariable = new Variable(); //создаём новую переменную и наделяем её новыми свойствами
+            MainVariable.Name = DeclaratedVariable.VariableName;
+            MainVariable.AlternativeName = "var_" + count.vars.ToString();
+            count.vars++;
+            parentBlock.BlockVariables.Add(MainVariable); //помещаем в список переменных данного блока
+            DeclaratedVariable.SetMainVariable(count);
+        }
+
+    }
+
+    public partial class ConditionalOperatorNode : BlockNode
+    {
+ 
+    }
+
+    public partial class ConditionalBranchNode : BlockNode//какая-то из веток if или else
+    {
+    }
+
+    public partial class CycleOperator : BlockNode
+    {
+        
+    }
+
+    public partial class AssignmentOperator : AtomNode
+    {
+        public override void SetMainVariable(Counters count)
+        {
+
+        }
+    }
+
+    public partial class MainRootNode : BlockNode
+    {
+    }
+
+    //С чтением и записью хз как, пока строка будет
+    public partial class ReadOperator : AtomNode
+    {
+       
+    }
+
+    public partial class WriteOperator : AtomNode
+    {
+    }
+}
